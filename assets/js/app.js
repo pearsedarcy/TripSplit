819416ad3f488a3e91006229fba11c930aeee485
pearsedarcy <pearse.darcy@outlook.com>
2024-02-26 01:57:59 +0100

    Add functionality to add friends to the friends list in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,49 @@ for (let i = 0; i < nav.length; i++) {
 });
 }
 };
+
+/*
+=== 1. Add Friend to the Friends list ===
+  - 1.1 - Get the add friends form
+  - 1.2 -Add event listener to the form
+  - 1.3 -Prevent the default form submission
+  - 1.4 -Get the add friends form name input, with the id of 'add-friends-input'
+  - 1.5 -Get the add friends form submit button, with the id of 'add-friend-input-plus'
+  - 1.6 -Get UL with the class of 'friends
+  - 1.7 -Create new list item
+  - 1.8 -Create text node for new friend
+  - 1.9 -Append new friend name to new friend list item
+  - 1.10 -Append new friend list item to form friend list
+  - 1.11 -Clear the input field
+    TODO Change the color of the input field border when clicked, focused
+    TODO Prevent empty form submission, add a validation to the form
+    TODO Add a delete button to the list items
+    TODO Add a message when the list is empty
+    TODO Limit the number of friends to add
+*/
+
+// -1.1 - 
+const addFriendsForm = document.getElementById('add-friends-form');
+// -1.2 -
+addFriendsForm.addEventListener('submit', submitFriendForm);
+// Main function to submit the form
+function submitFriendForm(event){
+  // -1.3 -
+  event.preventDefault();
+  // -1.4 -
+  const addFriendInput = document.querySelector('#add-friends-input');
+  //  -1.5 -
+  const addFriendInputBtn = document.querySelector('#add-friend-input-plus');
+  //  -1.6 -
+  const friendsList = document.querySelector('.friends-list');
+  // -1.7 -
+  const newFriend = document.createElement('li');
+  // -1.8 -
+  const newFriendName = document.createTextNode(addFriendInput.value);
+  // -1.9 -
+  newFriend.appendChild(newFriendName);
+  // -1.10 -
+  friendsList.appendChild(newFriend);
+  // -1.11 -
+  addFriendInput.value = '';
+}
