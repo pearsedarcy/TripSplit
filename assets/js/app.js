f8f89ab44c7d93c7cf2735cb7fdba79fb5e06032
pearsedarcy <pearse.darcy@outlook.com>
2024-02-26 17:39:03 +0100

    Add delete functionality to friend list elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,6 +27,19 @@ function submitFriendsForm(event) {
 function createFriendElement(name) {
   const newFriend = document.createElement('li');
   newFriend.textContent = name;
+  // Create a delete button
+  const deleteButton = document.createElement('a');
+  deleteButton.classList.add('delete-button');
+  deleteButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30">
+      <path d="M312-172q-25 0-42.5-17.5T252-232v-488h-40v-28h148v-28h240v28h148v28h-40v488q0 26-17 43t-43 17H312Zm368-548H280v488q0 14 9 23t23 9h336q12 0 22-10t10-22v-488ZM402-280h28v-360h-28v360Zm128 0h28v-360h-28v360ZM280-720v520-520Z"/>
+    </svg>
+  `;
+  deleteButton.addEventListener('click', () => {
+    newFriend.remove();
+    displayErrorMessage('');
+  });
+  newFriend.appendChild(deleteButton);
   return newFriend;
 }
 // Count the number of friends
